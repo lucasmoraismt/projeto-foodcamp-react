dfa0e50d4fbc3b3bef8c56369b66be072ba51f3d
Lucas de Morais <lucasmoraismt@gmail.com>
2021-05-07 20:07:01 -0300

    Entrega projeto

--- src/components/Container/Option.js
+++ src/components/Container/Option.js
@@ -1,14 +1,14 @@
 import Dish from "./Dish";
 
 export default function Option(props) {
-    const {array, p, selected, setSelected } = props;
+	const { array, p, selected, setSelected } = props;
 	return (
 		<div>
 			<p>{p}</p>
 			<ul>
 				{array.map((f) => (
 					<Dish
-                        key={f.title}
+						key={f.title}
 						img={f.img}
 						title={f.title}
 						description={f.description}
